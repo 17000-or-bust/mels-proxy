63d6fd2465758e6c928d6e03fd078621e07d1cd6
Greg Kittleson <greg.kittleson@gmail.com>
2019-02-22 20:44:12 -0800

    Implement proxy middleware to route EC2 API calls

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 let express = require('express');
-let morgan = require('morgan');
-let bodyParser = require('body-parser');
 const port = 8000;
 
 let app = express();
-
 app.use(express.static(__dirname + '/public'));
+
+let proxy = require('http-proxy-middleware');
+app.use(
+  '/api/reserve',
+  proxy({
+    target: 'http://ec2-18-191-229-0.us-east-2.compute.amazonaws.com',
+    changeOrigin: true
+  })
+);
+app.use(
+  '/api/photos',
+  proxy({
+    target: 'http://ec2-18-206-121-61.compute-1.amazonaws.com',
+    changeOrigin: true
+  })
+);
+app.use(
+  '/menu',
+  proxy({ target: 'http://localhost:3030', changeOrigin: true })
+);
+// app.use('/overview', proxy({ target: 'http://localhost:3000', changeOrigin: true }));
+// app.use('/overview', proxy({ target: 'http://localhost:3000', changeOrigin: true }));
+
+let morgan = require('morgan');
 app.use(morgan('dev'));
+let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
