4c9cfb0a0c8684c81f17916be73ac25e4074bcdb
Melsm650 <wp.developerhelpers@gmail.com>
2019-03-14 15:56:47 -0700

    "EC2 - Proxy update to render all services"

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('newrelic');
 let express = require('express');
 let compress = require('compression');
 const port = 8000;
@@ -8,13 +7,33 @@ app.use(compress());
 app.use(express.static(__dirname + '/public'));
 
 let proxy = require('http-proxy-middleware');
-
 app.use(
-  '/overview/:restaurantId',
-  proxy({
-    target: 'http://localhost:3000',
-    changeOrigin: true
-  })
+	'/api/reserve',
+	proxy({
+		target: 'http://ec2-13-59-134-78.us-east-2.compute.amazonaws.com/',
+		changeOrigin: true
+	})
+);
+app.use(
+	'/api/photos',
+	proxy({
+		target: 'http://ec2-54-159-114-20.compute-1.amazonaws.com/',
+		changeOrigin: true
+	})
+);
+app.use(
+	'/api/menu',
+	proxy({
+		target: 'http://ec2-18-222-202-91.us-east-2.compute.amazonaws.com/',
+		changeOrigin: true
+	})
+);
+app.use(
+	'/overview',
+	proxy({
+		target: 'http://ec2-18-191-110-131.us-east-2.compute.amazonaws.com:3000/',
+		changeOrigin: true
+	})
 );
 
 let morgan = require('morgan');
@@ -26,5 +45,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/:id', express.static(__dirname + '/public'));
 
 app.listen(port, () => {
-  console.log('Listening on port', port);
+	console.log('Listening on port', port);
 });
